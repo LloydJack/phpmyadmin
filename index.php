d50f18649b5c985b4a641aab038c1bee83fda912
1303788 <lloydjack1@hotmail.co.uk>
2015-10-26 15:56:24 +0000

    Yo there 5

---
+++ index.php
@@ -0,0 +1,95 @@
+
+/**
+ * Created by PhpStorm.
+ * User: 1303788
+ * Date: 19/10/2015
+ * Time: 15:27
+ */
+
+<!DOCTYPE html>
+<HTML>
+    <head>
+        </head>
+        <body>
+            <p>
+                <?php
+                /*echo "Hello, world!";
+                echo "Hello," . " " . "world" . "!";
+                echo 5 * 7;
+                $myname = "Frodo Baggins";
+                $myage = 111;
+                echo "My name is" . $myname . "and I am " . $myage;
+
+                echo "I get printed!";
+                // I don't! I'm a comment.
+
+                /* I don’t get printed either
+                and neither do I */
+                /*
+                $name = "Simon";
+
+                if ($name == "Simon") {
+                    print "I know you!";
+                }
+                else {
+                    print "Who are you?";
+                }
+                */
+
+                /*$myage = 15;
+
+                print "Lloyd ";
+                if ($myage >= 21) {
+                    print " is allowed to buy specs, mugs and sausage rolls";
+                }
+                else if ($myage >= 18) {
+                    print " is allowed to buy specs and mugs";
+                }
+                else if ($myage >= 16) {
+                    print " is allowed to buy specs";
+                }
+                else {
+                    print " is not allowed to buy an item";
+                }
+
+                $wantedgood = "specs";
+
+                switch ($wantedgood) {
+                    case "sausage rolls":
+                        echo "You have to be 21 to buy sausage rolls";
+                        break;
+                    case "mugs";
+                        echo "You have to be 18 to buy mugs";
+                        break;
+                    case "specs":
+                        echo "You have to be 16 to buy specs";
+                        break;
+                    default:
+                        echo "Not a valid option";
+                }
+
+                $provisionedActivities = array("Specs", "Drugs", "Rock and Roll");
+
+                foreach($provisionedActivities as $x) {
+                    print "<p>$x</p>";
+                }
+
+                $provisionedActivities[1] = "hugs";
+
+                foreach($provisionedActivities as $x) {
+                    print "<p>$x</p>";
+                }
+
+                unset($provisionedActivities[2]);
+
+                foreach($provisionedActivities as $x) {
+                    print "<p>$x</p>";
+                }
+                 */
+
+
+
+                ?>
+            </p>
+        </body>
+</HTML>
