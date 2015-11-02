702f47cc6f8fd493ca68063dc53040a30dc39c2b
1303788 <lloydjack1@hotmail.co.uk>
2015-11-02 15:01:42 +0000

    yo there 9

---
+++ helloPrinter.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1303788
+ * Date: 02/11/2015
+ * Time: 15:00
+ */
---
+++ helloSelector.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<HTML>
+<head>
+</head>
+<body>
+<p>
+    <?php
+    echo "Where would you like to say hello?";
+
+
+
+
+    ?>
+</p>
+</body>
+</HTML>
